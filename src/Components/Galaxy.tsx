e1be6002e5fe1450c8bcbf30fa2a74d98f07ab8e
mlnbk <dev.milan.bako@gmail.com>
2023-10-05 12:41:41 +0200

    feat: update universe
    * add constants file

--- src/Components/Galaxy.tsx
+++ src/Components/Galaxy.tsx
@@ -3,13 +3,13 @@ import { FC, useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { useFrame, useThree } from '@react-three/fiber';
 
-const spaceVals = [
-  { x: 0.0369, y: 0.1368, z: -0.2367 },
-  { x: -0.0963, y: 0.1962, z: 0.2961 },
-  { x: 0.1701, y: -0.267, z: 0.0369 },
-  { x: -0.1296, y: 0.2367, z: -0.1962 },
-  { x: 0.2367, y: -0.0369, z: 0.0963 },
-];
+import {
+  maxPlanetRadius,
+  maxStarRadius,
+  numOfStars,
+  planetPositions,
+  planetScreenFactor,
+} from '../constans';
 
 interface GalaxyProps {
   position: { x: number; y: number; z: number };
@@ -25,46 +25,22 @@ const Galaxy: FC<GalaxyProps> = ({
   speed = 1,
 }) => {
   const { size } = useThree();
-
-  useFrame(({ clock }) => {
-    if (planetContainer.current) {
-      const elapsedTime = clock.getElapsedTime();
-      const x = position.x + radius * Math.cos(speed * elapsedTime);
-      const z = position.z + radius * Math.sin(speed * elapsedTime);
-      planetContainer.current.position.set(x, position.y, z);
-    }
-  });
-
   const planetContainer = useRef<THREE.Group>(new THREE.Group());
+  const starsContainer = useRef<THREE.Group>(new THREE.Group());
+
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  planetContainer.current.add(ambientLight);
-  planetContainer.current.position.set(position.x, position.y, position.z);
-  const planet = new THREE.Object3D();
-  const geometry = new THREE.SphereGeometry(10, 32, 32);
-  const material = new THREE.MeshBasicMaterial({
-    color: 0x343a40,
-    wireframe: false,
-    transparent: false,
-  });
-
-  const sphere = new THREE.Mesh(geometry, material);
-  planet.add(sphere);
-
-  useEffect(() => {
-    generatePlanets();
-  }, [size]);
 
   const generatePlanets = () => {
     planetContainer.current.clear();
     const planetGroup = new THREE.Object3D();
-    const maxPlanetRadius = 0.02;
 
     for (let i = 0; i < planets.length; i++) {
       const planetColors = ['#FF5733', '#44D3A5', '#D144A5'];
-      const planetRadius = (0.9 + Math.random() * 0.9) * maxPlanetRadius;
+      const planetRadius = (0.95 + Math.random() * 0.95) * maxPlanetRadius;
 
-      const screenFactor = Math.min(size.width, size.height) * 0.001;
-      const { x, y, z } = spaceVals[i];
+      const screenFactor =
+        Math.min(size.width, size.height) * planetScreenFactor;
+      const { x, y, z } = planetPositions[i];
       const planetPosition = new THREE.Vector3(
         x * screenFactor,
         y * screenFactor,
@@ -85,13 +61,74 @@ const Galaxy: FC<GalaxyProps> = ({
     planetContainer.current.add(ambientLight);
   };
 
+  const generateStars = () => {
+    starsContainer.current.clear();
+
+    const starsGroup = new THREE.Object3D();
+
+    for (let i = 0; i < numOfStars; i++) {
+      const starRadius = (0.95 + Math.random() * 0.95) * maxStarRadius;
+      const starGeometry = new THREE.SphereGeometry(starRadius, 16, 16);
+      const starMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+      const starMesh = new THREE.Mesh(starGeometry, starMaterial);
+
+      const radius = Math.random() * 0.1;
+      const theta = Math.random() * Math.PI * 2;
+      const phi = Math.random() * Math.PI - Math.PI / 2;
+      starMesh.position.set(
+        radius * Math.cos(theta) * Math.cos(phi),
+        radius * Math.sin(phi),
+        radius * Math.sin(theta) * Math.cos(phi),
+      );
+      if (position.x <= 0) {
+        starMesh.rotateX(Math.PI / 3);
+      } else {
+        starMesh.rotateX(-Math.PI / 3);
+      }
+
+      starsGroup.add(starMesh);
+    }
+    starsContainer.current.add(starsGroup);
+    planetContainer.current.add(ambientLight);
+  };
+
+  useEffect(() => {
+    generatePlanets();
+    generateStars();
+  }, [size]);
+
+  useFrame(({ clock }) => {
+    if (planetContainer.current) {
+      const elapsedTime = clock.getElapsedTime();
+      const x = position.x + radius * Math.cos(speed * elapsedTime);
+      const y = position.y + radius * 0.2 * Math.sin(speed * elapsedTime);
+      const z = position.z + radius * Math.sin(speed * elapsedTime);
+      planetContainer.current.position.set(x, y, z);
+    }
+    if (starsContainer.current) {
+      const elapsedTime = clock.getElapsedTime();
+      const x = position.x + (radius / 2) * Math.cos(speed * elapsedTime);
+      const y = position.y + (radius / 2) * 0.2 * Math.sin(speed * elapsedTime);
+      const z = position.z + (radius / 2) * Math.sin(speed * elapsedTime);
+      starsContainer.current.position.set(x, y, z);
+    }
+  });
+
   return (
-    <group
-      ref={planetContainer}
-      position={[position.x, position.y, position.z]}
-    >
-      <primitive object={planetContainer.current} />
-    </group>
+    <>
+      <group
+        ref={planetContainer}
+        position={[position.x, position.y, position.z]}
+      >
+        <primitive object={planetContainer.current} />
+      </group>
+      <group
+        ref={starsContainer}
+        position={[position.x, position.y, position.z]}
+      >
+        <primitive object={starsContainer.current} />
+      </group>
+    </>
   );
 };
 
--- src/Pages/Universe.tsx
+++ src/Pages/Universe.tsx
@@ -1,23 +1,26 @@
 import { FC, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 
+import {
+  galaxyOrbitSpeeds,
+  galaxyScreenFactor,
+  galaxyPositions as positions,
+} from '../constans';
 import Galaxy from '../Components/Galaxy';
 
 type GalaxyPosition = { x: number; y: number; z: number };
 
-const positions = [
-  { x: -2, y: 1, z: 0 },
-  { x: 2, y: 2, z: 0 },
-  { x: -1.2, y: -2, z: 0 },
-  { x: 1.8, y: -1, z: 0 },
-  { x: 0, y: 0, z: 0 },
-];
 const Universe: FC = () => {
   const [galaxyPositions, setGalaxyPositions] = useState<GalaxyPosition[]>([]);
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    setShowText(true);
+  }, []);
 
   const updateGalaxyPositions = () => {
     const screenFactor =
-      Math.min(window.innerWidth, window.innerHeight) * 0.0015;
+      Math.min(window.innerWidth, window.innerHeight) * galaxyScreenFactor;
     setGalaxyPositions(
       positions.map((position) => ({
         x: position.x * screenFactor,
@@ -39,15 +42,25 @@ const Universe: FC = () => {
 
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <div className="text-white">Welcome to My Coding Universe!</div>
-      <Canvas camera={{ position: [0, 0, 5] }}>
+      <div
+        className={`absolute transform h-full w-full ${
+          showText ? 'translate-y-4 opacity-100' : 'translate-y-1/2 opacity-0'
+        } text-white text-center transition-all duration-3000 ease-in-out`}
+      >
+        <h1 className="text-5xl font-extrabold">Cosmic Odyssey</h1>
+        <p className="text-xl mt-4">
+          Journey Through My Interests and Projects
+        </p>
+      </div>
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <group>
           {galaxyPositions.map((position, index) => (
             <Galaxy
               key={index}
               position={position}
               planets={Array.from({ length: 5 })}
-              radius={0.45}
+              radius={0.1}
+              speed={galaxyOrbitSpeeds[index]}
             />
           ))}
         </group>
