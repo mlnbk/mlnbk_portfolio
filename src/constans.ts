752c72fddc50c932285d8bd8106d7e9cd0968c6c
mlnbk <dev.milan.bako@gmail.com>
2023-10-10 23:32:49 +0200

    chore: add galaxies and projects

--- src/constans.ts
+++ src/constans.ts
@@ -49,22 +49,105 @@ export const numOfStars = 500;
 
 export const galaxyData: Galaxy[] = [
   {
-    name: 'statistics',
-    title: 'Statistics',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis vel turpis efficitur bibendum. Proin viverra libero vitae suscipit.',
+    name: 'lifestyle_travel',
+    title: 'Lifestyle / Traveling',
+    description: 'Projects related to lifestyle and traveling.',
     projects: [
       {
-        title: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet.',
+        title: 'getaway-plan-react-client',
+        description:
+          'A React-based frontend for the GetawayPlan app, which allows users to plan their trips easily.',
       },
       {
-        title: 'Project 2',
-        description: 'Consectetur adipiscing elit.',
+        title: 'getaway-plan-api',
+        description:
+          'A Node.js-based API for the GetawayPlan app, which provides endpoints for user authentication, trip planning, and more.',
+      },
+      {
+        title: 'getaway-plan-infrastructure',
+        description:
+          'Infrastructure for the getaway-plan-react-client and getaway-plan-api applications.',
       },
     ],
-    color: {
-      colorRep: 0x38bef8,
-    },
+    color: { colorRep: 0x94a684 }, // green
+  },
+  {
+    name: 'machine_learning',
+    title: 'Machine Learning',
+    description: 'Projects related to machine learning and data science.',
+    projects: [
+      {
+        title: 'bulldozer_price',
+        description:
+          "Bulldozer price supervised learning time series data project based on the Complete Machine Learning & Data Science Bootcamp Udemy course's related section.",
+      },
+      {
+        title: 'heart_disease',
+        description:
+          "Heart disease supervised learning classification project based on the Complete Machine Learning & Data Science Bootcamp Udemy course's related section.",
+      },
+      {
+        title: 'neural_networks',
+        description:
+          "Neural networks (deep learning, transfer learning, TensorFlow 2) practice project based on the Complete Machine Learning & Data Science Bootcamp Udemy course's related section.",
+      },
+    ],
+    color: { colorRep: 0xbeadfa }, // pastel purple
+  },
+  {
+    name: 'data_analysis',
+    title: 'Data Analysis',
+    description: 'Projects related to data analysis and data science.',
+    projects: [
+      {
+        title: 'numpy_exercise',
+        description:
+          "Numpy exercise project based on the Complete Machine Learning & Data Science Bootcamp Udemy course's related section.",
+      },
+      {
+        title: 'matplotlib_exercise',
+        description:
+          "Matplotlib exercise project based on the Complete Machine Learning & Data Science Bootcamp Udemy course's related section.",
+      },
+      {
+        title: 'scikit_learn_exercise',
+        description:
+          "Scikit-Learn exercise project based on the Complete Machine Learning & Data Science Bootcamp Udemy course's related section.",
+      },
+    ],
+    color: { colorRep: 0xea907a }, // pastel orange/red
+  },
+  {
+    name: 'mobile_apps',
+    title: 'Mobile Apps',
+    description: 'Mobile app projects developed using Flutter & Dart.',
+    projects: [
+      {
+        title: 'chat_app',
+        description:
+          "Chat app based on the Flutter & Dart - The Complete Guide Udemy course's related section.",
+      },
+      {
+        title: 'favorite_places',
+        description:
+          "Favorite Places app based on the Flutter & Dart - The Complete Guide Udemy course's related section.",
+      },
+      {
+        title: 'shopping_list',
+        description:
+          "Shopping list app based on the Flutter & Dart - The Complete Guide Udemy course's related section.",
+      },
+      {
+        title: 'meals_app',
+        description:
+          "Meals app based on the Flutter & Dart - The Complete Guide Udemy course's related section.",
+      },
+      {
+        title: 'expense_tracker',
+        description:
+          "Expense Tracker project based on the Flutter & Dart - The Complete Guide Udemy course's related section.",
+      },
+    ],
+    color: { colorRep: 0x6096b4 }, // pastel blue
   },
 ];
