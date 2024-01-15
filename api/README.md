837283cb3616b07221325b1c2f974c4f900113f1
mlnbk <dev.milan.bako@gmail.com>
2024-01-15 11:52:33 +0100

    feat: integrate `nestjs-github-activity`
    * add `@nestjs/config` module
    * init GithubActivity module, add dummy endpoint
    * README.md updated

--- api/README.md
+++ api/README.md
@@ -8,6 +8,20 @@ This is the API for the Cosmic Code Showcase portfolio, built with NestJS. It is
 $ npm install
 ```
 
+## Note on nestjs-github-activity package
+
+The `nestjs-github-activity` package is sourced from GitHub.
+
+You can find it [here](https://github.com/mlnbk/nestjs-github-activity).
+
+After cloning the repository, it needs to be built using `npm run build`.
+
+Once built, it can be locally linked to your project using `npm link` in the source app.
+
+Then, navigate to the `api` directory of the this application where you want to use this package. Run `npm link nestjs-github-activity`.
+
+This will create a symbolic link from the local `node_modules` folder to the global package.
+
 ## Running the app
 
 ```bash
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { GithubActivityModule } from 'nestjs-github-activity';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GithubActivityModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        userName: 'mlnbk',
+        githubPersonalAccessToken: configService.get(
+          'GITHUB_PERSONAL_ACCESS_TOKEN',
+        ),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
