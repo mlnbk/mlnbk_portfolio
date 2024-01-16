d6cc25d81ef6f2654e952fbc1379a7fdf894b4f3
mlnbk <dev.milan.bako@gmail.com>
2024-01-16 12:05:00 +0100

    feat: add CORS to API

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: configService.get('WEB_URL'),
+    methods: 'GET',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3005);
 }
 bootstrap();
