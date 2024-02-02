1e6b1bac6fb603230de0124118aa48ee1759b536
mlnbk <dev.milan.bako@gmail.com>
2024-02-02 14:56:55 +0100

    feat: add email sending to API
    * send mails using nodemailer

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,16 +1,21 @@
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 import {
   GithubActivityResponse,
   GithubActivityService,
 } from 'nestjs-github-activity';
 
+import { SendContactEmailBody } from './types';
+
 @Controller()
 export class AppController {
   constructor(
-    private readonly githubService: GithubActivityService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly configService: ConfigService,
+    private readonly githubService: GithubActivityService,
   ) {}
 
   @Get('/github-activities')
@@ -24,5 +29,30 @@ export class AppController {
     }
     await this.cacheManager.set(cacheKey, activities);
     return activities;
+  }
+
+  @Post('/contact')
+  async sendContactEmail(@Body() body: SendContactEmailBody) {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.configService.get('GMAIL_USERNAME'),
+        pass: this.configService.get('GMAIL_PASSWORD'),
+      },
+    });
+
+    const mailOptions = {
+      from: 'Portfolio Contact',
+      to: this.configService.get('GMAIL_USERNAME'),
+      subject: `New contact from ${body.name}`,
+      text: `New contact form submission from ${body.name} with email ${body.email}. \nMessage: ${body.message}`,
+    };
+
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      console.log('Email sent: ' + info.response);
+    } catch (error) {
+      throw new Error('Failed to send email');
+    }
   }
 }
